1c2826efff5f15a9dea490e007c095e8e63263a2
su-mong <sumong.woo@ourtube.co.kr>
2023-10-20 20:28:19 +0900

    최종본 : 로그 추가

--- src/metadata/metadata.service.ts
+++ src/metadata/metadata.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { MetadataRepository } from './metadata.repository';
 import { MetadataDto } from './metadata.dto';
 
 @Injectable()
 export class MetadataService {
+  private readonly logger = new Logger(MetadataService.name);
+
   constructor(private readonly metadataRepository: MetadataRepository) {}
 
   // 전체 메타데이터 가져오기
   async getAllMetadata(): Promise<MetadataDto> {
+    this.logger.debug(`🚨 getAllMetadata called`);
     const data = await this.metadataRepository.getAllMetadata();
     return new MetadataDto(data.length, data);
   }
